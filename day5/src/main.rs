8a96017992a1ecfff8fcfe107e418e17df6d603d
Nikola Đipanov <nikola.djipanov@gmail.com>
2020-12-05 09:35:49 +0000

    day5 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::io::{self, Read};
 use string_error::*;
@@ -15,10 +16,10 @@ fn parse_range(code: &str, s: usize, e: usize) -> Option<usize> {
         match step {
             'F' | 'L' => end = start + (end - start) / 2,
             'B' | 'R' => start = start + (end - start + 1) / 2,
-            _ => break
+            _ => break,
         };
         //println!("{}, {}", start, end)
-    };
+    }
     if end == start {
         Some(end)
     } else {
@@ -33,22 +34,37 @@ impl BoardingPass {
         let col = parse_range(col_code, 0, 7)?;
         Some(BoardingPass { row, col })
     }
-    
+
     fn seat_id(&self) -> usize {
         self.row * 8 + self.col
     }
 }
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer);
+    io::stdin().read_to_string(&mut buffer)?;
     let passes = buffer
         .trim_end()
         .split("\n")
         .map(|code| BoardingPass::parse(&code))
         .collect::<Option<Vec<BoardingPass>>>()
         .ok_or(new_err("Failed parsing"))?;
-    println!("Max pass: {:?}", passes.iter().map(|pass| pass.seat_id()).max());
+    let found_ids = passes
+        .iter()
+        .map(|pass| pass.seat_id())
+        .collect::<Vec<usize>>();
+    println!("Max pass: {:?}", found_ids.iter().max());
+    let all_ids = (1..127)
+        .map(|row| (1..8).map(move |col| row * 8 + col))
+        .flatten()
+        .collect::<HashSet<usize>>();
+    let found_ids_set = found_ids.into_iter().collect::<HashSet<usize>>();
+    println!(
+        "Missing ids {:?}",
+        all_ids
+            .difference(&found_ids_set)
+            .filter(|&id| found_ids_set.contains(&(id - 1)) && found_ids_set.contains(&(id + 1)))
+            .collect::<HashSet<&usize>>()
+    );
     Ok(())
 }
