acc632df313a4150fc95b595fe5dd844baed5d07
Nikola Đipanov <nikola.djipanov@gmail.com>
2020-12-06 12:04:08 +0000

    day6 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,13 +7,40 @@ fn group_answers(answers: &str) -> usize {
     uniq.len()
 }
 
+fn group_answers2(answers: &str) -> usize {
+    let mut person_answers = answers.split("\n");
+    if let Some(fst) = person_answers.next() {
+        let mut common: HashSet<char> = fst.chars().collect();
+        for ans in person_answers {
+            common = common
+                .intersection(&ans.chars().collect())
+                .copied()
+                .collect();
+        }
+        common.len()
+    } else {
+        0
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
-    let input = buffer
-        .split("\n\n")
-        .map(|l| str::replace(l, "\n", ""))
-        .collect::<Vec<String>>();
-    println!("Sum of yes answers {}", input.iter().map(|group| group_answers(&group)).sum::<usize>());
+    let input = buffer.split("\n\n").collect::<Vec<&str>>();
+
+    println!(
+        "Sum of yes answers {}",
+        input
+            .iter()
+            .map(|group| { group_answers(&str::replace(group, "\n", "")) })
+            .sum::<usize>()
+    );
+    println!(
+        "Sum of common yes answers {}",
+        input
+            .iter()
+            .map(|group| group_answers2(group))
+            .sum::<usize>()
+    );
     Ok(())
 }
