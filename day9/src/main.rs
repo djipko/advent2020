70c4c04d21863bb603100d440567a97a0fa1de43
Nikola Đipanov <nikola.djipanov@gmail.com>
2020-12-09 09:45:48 +0000

    day9 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,25 +1,56 @@
 use combinations::Combinations;
-use std::collections::{VecDeque, HashSet};
+use std::collections::{HashSet, VecDeque};
 use std::error::Error;
 use std::io::{self, Read};
 
-fn valid_next(input: Vec<u64>) -> HashSet<u64> {
-    Combinations::new(input, 2).map(|c| c.iter().sum()).collect()
+fn valid_next(input: Vec<&u64>) -> HashSet<u64> {
+    Combinations::new(input, 2)
+        .map(|c| c.iter().cloned().sum())
+        .collect()
+}
+
+fn find_weak(input: &Vec<u64>) -> Option<u64> {
+    let mut current: VecDeque<u64> = input.iter().cloned().take(25).collect();
+    for &next in input[25..].iter() {
+        if !valid_next(current.iter().collect()).contains(&next) {
+            return Some(next);
+        }
+        current.pop_back();
+        current.push_front(next);
+    }
+    None
+}
+
+fn enc_weakness(input: &Vec<u64>, weak: u64) -> Option<u64> {
+    for n in 2..(input.len()) {
+        let mut cur: VecDeque<u64> = input.iter().take(n).cloned().collect();
+        let mut sum: u64 = cur.iter().sum();
+        for &next in input[n..].iter() {
+            //println!("{}: {:?}", sum, cur);
+            if sum == weak {
+                return Some(*cur.iter().min().unwrap() + *cur.iter().max().unwrap());
+            }
+            sum -= cur.pop_front().unwrap();
+            sum += next;
+            cur.push_back(next);
+        }
+    }
+    None
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
-    let input = buffer.trim_end().split("\n").map(
-        |n| n.parse::<u64>()).collect::<Result<Vec<u64>, _>>()?;
-    let mut current: VecDeque<u64> = input.iter().cloned().take(25).collect();
-    for &next in input[25..].iter() {
-        if !valid_next(current.iter().cloned().collect()).contains(&next) {
-            println!("Found: {}", next);
-            break;
+    let input = buffer
+        .trim_end()
+        .split("\n")
+        .map(|n| n.parse::<u64>())
+        .collect::<Result<Vec<u64>, _>>()?;
+    if let Some(weak) = find_weak(&input) {
+        println!("Found: {}", weak);
+        if let Some(enc_weak) = enc_weakness(&input, weak) {
+            println!("Enc weakness: {}", enc_weak);
         }
-        current.pop_back();
-        current.push_front(next);
     }
 
     Ok(())
