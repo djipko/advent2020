8b04e11a48dba41d3c9efcddd1df72876f31c396
Nikola Đipanov <nikola.djipanov@gmail.com>
2020-12-16 18:16:45 +0000

    day16 2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::io::{self, Read};
 use std::ops::RangeInclusive;
@@ -9,6 +9,20 @@ type Ticket = Vec<usize>;
 #[derive(Debug)]
 struct Rules {
     rules: HashMap<String, Vec<RangeInclusive<usize>>>,
+    field_mapping: HashMap<String, usize>,
+}
+
+struct Multizip<T>(Vec<T>);
+
+impl<T> Iterator for Multizip<T>
+where
+    T: Iterator,
+{
+    type Item = Vec<T::Item>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.iter_mut().map(Iterator::next).collect()
+    }
 }
 
 impl Rules {
@@ -17,9 +31,7 @@ impl Rules {
         let data = input
             .iter()
             .map(|line| {
-                println!("{}", line);
                 if let Some(caps) = rule_re.captures(line) {
-                    println!("{:?}", caps);
                     let fst_range = caps[2].parse().ok()?..=caps[3].parse().ok()?;
                     let snd_range = caps[4].parse().ok()?..=caps[5].parse().ok()?;
                     Some((caps[1].to_string(), vec![fst_range, snd_range]))
@@ -31,6 +43,7 @@ impl Rules {
 
         Some(Rules {
             rules: data.into_iter().collect(),
+            field_mapping: HashMap::new(),
         })
     }
 
@@ -38,12 +51,44 @@ impl Rules {
         let mut ret = None;
         for n in ticket.iter() {
             if !self.rules.values().flatten().any(|r| r.contains(n)) {
-                println!("Invalid: {:?}, {}", ticket, n);
                 ret = Some(*n);
                 break;
             }
         }
         ret
+    }
+
+    fn build_field_mapping(&mut self, valid: &Vec<Ticket>) {
+        let valid_it = valid.iter().map(|v| v.iter()).collect();
+        let candidates = &mut HashMap::new();
+        for (pos, codes) in Multizip(valid_it).enumerate() {
+            let matched = self
+                .rules
+                .iter()
+                .filter(|(_, rule)| {
+                    codes
+                        .iter()
+                        .all(|code| rule.iter().any(|r| r.contains(code)))
+                })
+                .map(|(fld, _)| fld.to_string())
+                .collect::<HashSet<String>>();
+            candidates.insert(pos, matched);
+        };
+        while candidates.len() > 0 {
+            let found_candidates: Vec<(usize, HashSet<String>)> = candidates
+                .clone()
+                .into_iter()
+                .filter(|(_, flds)| flds.len() == 1)
+                .collect();
+            for (pos, flds) in found_candidates {
+                let fld = flds.iter().next().unwrap().to_string();
+                self.field_mapping.insert(fld.clone(), pos);
+                candidates
+                    .iter_mut()
+                    .for_each(|(_, c)| drop(c.remove(&fld)));
+                candidates.remove(&pos);
+            }
+        }
     }
 }
 
@@ -63,16 +108,31 @@ fn main() -> Result<(), Box<dyn Error>> {
     let my_ticket_str = input_parts.next().ok_or("My ticket not found")?;
     let tickets_str = input_parts.next().ok_or("Tickets not found")?;
 
-    let rules = Rules::parse(rule_str.split("\n").collect()).ok_or("Failed parsing")?;
+    let mut rules = Rules::parse(rule_str.split("\n").collect()).ok_or("Failed parsing")?;
     let tickets = tickets_str
         .split("\n")
         .skip(1)
         .map(|ts| parse_ticket(ts))
         .collect::<Option<Vec<Ticket>>>()
         .ok_or("Failed parsing tix")?;
-    println!("Err rate: {}",
-        tickets.iter().map(|t| rules.find_invalid(t).unwrap_or(0)).sum::<usize>()
+    println!(
+        "Err rate: {}",
+        tickets
+            .iter()
+            .map(|t| rules.find_invalid(t).unwrap_or(0))
+            .sum::<usize>()
     );
+    let valid_tix = tickets
+        .iter()
+        .cloned()
+        .filter(|t| rules.find_invalid(t) == None)
+        .collect();
+    rules.build_field_mapping(&valid_tix);
+    let my_ticket = parse_ticket(my_ticket_str.split("\n").nth(1).unwrap()).ok_or("Failed parsing my ticket")?;
+    let res: usize = rules.field_mapping.iter()
+        .filter(|(fld, _)| fld.starts_with("departure"))
+        .map(|(_, pos)| my_ticket[*pos]).product();
+    println!("Res: {}", res);
 
     Ok(())
 }
